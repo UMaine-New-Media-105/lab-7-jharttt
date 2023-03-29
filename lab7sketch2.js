16dac354c072e4c9890a44133ddbc12235ae6fcb
jharttt <124187324+jharttt@users.noreply.github.com>
2023-03-29 13:05:47 -0400

    Create lab7sketch2.js
    https://editor.p5js.org/john.hart/sketches/zxITkIt0s

---
+++ lab7sketch2.js
@@ -0,0 +1,23 @@
+function setup() {
+  let offset = 40;
+  createCanvas(400, 400);
+  background(200);
+
+// used lerpcolor to make gradient easier
+  let fromColor = color(255, 165, 0);
+  let toColor = color(0, 255, 255);
+
+
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let x = offset + j * offset * 2;
+      let y = offset + i * offset * 2;
+      let gradientAmountX = j / 4;
+      let gradientAmountY = i / 4;
+      let fillColor = lerpColor(fromColor, toColor, gradientAmountX + gradientAmountY);
+
+      fill(fillColor);
+      ellipse(x, y, 80, 80);
+    }
+  }
+}
